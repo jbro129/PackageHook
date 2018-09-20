b3a40780d7c272fd25e003388d6356bb55822c4c
Jbro129 <jonahsallen@gmail.com>
2018-09-19 22:54:43 -0400

    Fixed

--- PackageHook/app/src/main/java/com/jbro129/packagehook/MainApp.java
+++ PackageHook/app/src/main/java/com/jbro129/packagehook/MainApp.java
@@ -4,15 +4,17 @@ import android.app.Application;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.Signature;
+import android.util.Log;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
-public class MainApp extends Application implements InvocationHandler {
-    private static final int FLAG_0 = 0x0;
+public class MainApp extends Application implements InvocationHandler{
 
     private Object base;
+    private String pkg = "";
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -20,21 +22,22 @@ public class MainApp extends Application implements InvocationHandler {
         super.attachBaseContext(base);
     }
 
+
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+    {
         if ("getPackageInfo".equals(method.getName())) {
             String pkgName = (String) args[0];
             Integer flag = (Integer) args[1];
-            if ((flag & FLAG_0) != 0 && getPackageName().equals(pkgName))
-            {
+            if (pkg.equals(pkgName)) {
                 PackageInfo info = (PackageInfo) method.invoke(base, args);
 
                 // Set Info here
 
-                info.packageName = "package.name.has.been.changed";
-                info.versionName = "987654321.0";
-                info.versionCode = 987654321;
-
+                info.versionName = "121.0"; //  <---
+                info.versionCode = 121; //  <---
+                info.packageName = "com.package.name"; //  <---
+                
                 //
 
                 return info;
@@ -46,8 +49,30 @@ public class MainApp extends Application implements InvocationHandler {
     private void hook(Context context) {
         try
         {
+            Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+            Method currentActivityThreadMethod =
+                    activityThreadClass.getDeclaredMethod("currentActivityThread");
+            Object currentActivityThread = currentActivityThreadMethod.invoke(null);
+
+            Field sPackageManagerField = activityThreadClass.getDeclaredField("sPackageManager");
+            sPackageManagerField.setAccessible(true);
+            Object sPackageManager = sPackageManagerField.get(currentActivityThread);
+
+            Class<?> iPackageManagerInterface = Class.forName("android.content.pm.IPackageManager");
+            this.base = sPackageManager;
+            this.pkg = context.getPackageName();
+
+            Object proxy = Proxy.newProxyInstance(
+                    iPackageManagerInterface.getClassLoader(),
+                    new Class<?>[]{iPackageManagerInterface},
+                    this);
+
+            sPackageManagerField.set(currentActivityThread, proxy);
+
             Class<?> pInfo = Class.forName("android.content.pm.PackageInfo");
             Object pInstance = pInfo.newInstance();
+
+            // Set info here as well
 
             Field versionName = pInfo.getField("versionName");
             versionName.setAccessible(true);
@@ -58,11 +83,11 @@ public class MainApp extends Application implements InvocationHandler {
             Field packageName = pInfo.getField("packageName");
             packageName.setAccessible(true);
 
-            //Set info here as well
+            sPackageManagerField.set(currentActivityThread, proxy);
 
-            versionName.set(pInstance, "987654321.0");
-            versionCode.set(pInstance, 987654321);
-            packageName.set(pInstance, "package.name.has.been.changed");
+            versionName.set(pInstance, "121.0"); //  <---
+            versionCode.set(pInstance, 121); //  <---
+            packageName.set(pInstance, "com.package.name"); // <---
 
             //
 
